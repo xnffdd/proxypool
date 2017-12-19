38ed4f925d52ddafafac2108260c6224412096d4
lsdir <498541859@qq.com>
2017-12-19 20:30:30 +0800

    快代理 破解反爬 javascript生成加密cookies

--- spider/plugin/kuaiproxy.py
+++ spider/plugin/kuaiproxy.py
@@ -6,6 +6,7 @@ import time
 import logging
 
 import retrying
+import execjs
 
 from config import SPIDER_MAX_ATTEMPT_NUMBER
 from util.http import headers
@@ -27,12 +28,17 @@ class Proxy(Plugin):
         self.url_template = 'http://www.kuaidaili.com/free/inha/{page}/'
         self.re_ip_pattern = re.compile(r">(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})</td>")
         self.re_port_pattern = re.compile(r">(\d{1,5})</td>")
+        self.cookie = None
 
     @retrying.retry(stop_max_attempt_number=SPIDER_MAX_ATTEMPT_NUMBER)
     def _extract_proxy(self, page_num):
         try:
             full_url = self.url_template.format(page=page_num)
-            rp = self.session.get(url=full_url, headers=headers(host=self.host), timeout=10)
+            rp = self.session.get(url=full_url, headers=headers(host=self.host, cookie=self.cookie), timeout=10)
+            if rp.status_code == 521:
+                self._set_cookies(rp.text)
+                self._log(logger, 'extract data error', full_url, 'response javascript code')
+                self._need_retry()
         except Exception as e:
             self._log(logger, 'request error', full_url, str(e))
             self._need_retry()
@@ -53,6 +59,25 @@ class Proxy(Plugin):
 
         return [{'host': host, 'port': port, 'from': self.name, 'grab_time': get_current_time_str()} for host, port in
                 result_list]
+
+    def _set_cookies(self, text):
+        try:
+            js_function_name = re.findall(r'setTimeout\("(.*?)\(', text)[0]
+            js_args_value = re.findall(r'setTimeout\(.*?\((.*?)\)', text)[0]
+            js_code = re.findall(r'(function.*?)</script>', text)[0].replace('eval("qo=eval;qo(po);");', 'return po;')
+        except Exception as e:
+            logger.error('parse javascript code failed, error: %s' % str(e))
+            return
+
+        try:
+            exe = execjs.compile(js_code)
+            ret_text = exe.call(js_function_name, js_args_value)
+            cookies_str = re.findall(r"='(.*?);", ret_text)[0]
+        except Exception as e:
+            logger.error('execute javascript code failed, error: %s' % str(e))
+            return
+
+        self.cookie = cookies_str
 
     def start(self):
         for page in range(1, 10):
